dc61a4b5b7d18fb18b7e9f798aa45eefc008742c
IvanBrkicNeuralab <ivan@neuralab.net>
2025-10-30 12:36:15 +0100

    cookie

--- wp-content/themes/pg/assets/src/theme/js/main.js
+++ wp-content/themes/pg/assets/src/theme/js/main.js
@@ -87,7 +87,7 @@ import 'vanilla-cookieconsent/dist/cookieconsent.css';
 import * as CookieConsent from 'vanilla-cookieconsent/dist/cookieconsent.umd.js';
 
 let cookieEnable = false;
-if (!cookieEnable) {
+if (cookieEnable) {
 	window.cc = CookieConsent;
 	window.cc.run({
 
