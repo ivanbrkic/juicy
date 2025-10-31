7c4b8ea0b7616bc6676eb3ba6f7168a2c3f718d2
IvanBrkicNeuralab <ivan@neuralab.net>
2025-10-31 22:18:37 +0100

    ai update

--- wp-content/themes/pg/front-page.php
+++ wp-content/themes/pg/front-page.php
@@ -2,21 +2,21 @@
 	<div class="wrapper">
 	<div class="hero__cols">
 		<div class="hero__col-1">
-			<img class="hero__image-1" src="/wp-content/themes/pg/assets/static/img/family-s.png" alt="">
+			<img class="hero__image-1" src="/wp-content/themes/pg/assets/static/img/family-c.jpg" alt="">
 		</div>
 		<div class="text-center mw-555 mx-auto hero__col-2">
 		<img class="logo logo--big mb-2" src="/wp-content/themes/pg/assets/static/img/logo.svg" alt="">
 	<h1 class="text-yellow title-1 mb-2">Zagrij se za blagdane uz Juicy džempere</h1>
 	<p class="text-yellow mb-2">Ovih blagdana Juicy će se pobrinuti da čak i nespretni trenuci postanu lijepe uspomene i da te grije najtopliji božićni džemper. </p>
 	<div class="my-3 d-lg-none">
-		<img class="hero__image-3" src="/wp-content/themes/pg/assets/static/img/snowman-s.png" alt="">
+		<img class="hero__image-3" src="/wp-content/themes/pg/assets/static/img/snowman-c.png" alt="">
 	</div>	
 	<p class="mb-2">Podijeli s nama svoje omiljene blagdanske trenutke i osvoji genijalne pletene džempere. Fotkaj Juicy pored mamine zagorene purice, susjedovog rastopljenog snjegovića ili bakinih raščupanih čupavaca i osvoji lude pletene džempere za cijelu ekipu!</p>
 <p class="text-yellow p-bigger">Natječaj traje od 1. 11. do 10. 12. 2025. </p>
 
 		</div>
 		<div class="hero__col-3">
-			<img class="hero__image-2" src="/wp-content/themes/pg/assets/static/img/snowman-s.png" alt="">
+			<img class="hero__image-2" src="/wp-content/themes/pg/assets/static/img/snowman-c.png" alt="">
 		</div>
 	</div>
 
